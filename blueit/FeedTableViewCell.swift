7f5841e7e92468fdd79e21235466303fa3d56afe
ZoroPk <99301578+ZoroPk@users.noreply.github.com>
2022-11-22 16:54:45 -0800

    added Feed screen
    Added feed screen to story board and fixed navigation

---
+++ blueit/FeedTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedTableViewCell.swift
+//  blueit
+//
+//  Created by Mac Mini on 11/22/22.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
