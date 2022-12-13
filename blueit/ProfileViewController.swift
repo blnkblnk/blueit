805c1478903e57ae10915ec04e16cd60986748b2
XAlienAssassin <orionmedina557@gmail.com>
2022-12-13 14:27:01 -0500

    added profile

---
+++ blueit/ProfileViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ProfileViewController.swift
+//  blueit
+//
+//  Created by Orion Medina on 12/13/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    
+    @IBOutlet weak var ProfileImageView: UIImageView!
+    @IBOutlet weak var ProfileUsername: UILabel!
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadUserData()
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadUserData()
+        // Do any additional setup after loading the view.
+    }
+    
+    func loadUserData(){
+        
+        
+        
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
