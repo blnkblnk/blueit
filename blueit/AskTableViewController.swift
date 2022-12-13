284fb8c4f088b50571d4b2fc13c0c8c1c200b9b4
ZoroPk <99301578+ZoroPk@users.noreply.github.com>
2022-12-13 13:05:01 -0800

    added files for askreddit

---
+++ blueit/AskTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AskTableViewController.swift
+//  blueit
+//
+//  Created by Mac Mini on 12/13/22.
+//
+
+import UIKit
+
+class AskTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
