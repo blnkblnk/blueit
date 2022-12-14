5769aedbac6f2319dec4802d63b2c484f64b0687
ZoroPk <99301578+ZoroPk@users.noreply.github.com>
2022-12-14 10:01:38 -0800

    added ui for profile
    Added coins

--- blueit/ProfileViewController.swift
+++ blueit/ProfileViewController.swift
@@ -13,7 +13,9 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var ProfileImageView: UIImageView!
     @IBOutlet weak var ProfileUsername: UILabel!
     
+    @IBOutlet weak var Age: UILabel!
     
+    @IBOutlet weak var coins: UILabel!
     @IBOutlet weak var Karma: UILabel!
     
     override func viewDidAppear(_ animated: Bool) {
@@ -39,6 +41,10 @@ class ProfileViewController: UIViewController {
             let userlikes = user?["total_karma"] as? Int ?? 0
             print(user?["total_karma"] as Any)
             self.Karma.text = String(userlikes)
+            let accage = user?["created"] as? Int ?? 0
+            self.Age.text = String(accage)
+            let accoins = user?["coins"] as? Int ?? 0
+            self.coins.text = String(accoins)
             print(user?.keys as Any)
         }
     
