b4d8aae959c65cc7a3178e08f22ed5d37537651a
ZoroPk <99301578+ZoroPk@users.noreply.github.com>
2022-12-13 16:37:12 -0800

    added view controller for select

---
+++ blueit/selectViewController.swift
@@ -0,0 +1,36 @@
+//
+//  selectViewController.swift
+//  blueit
+//
+//  Created by Mac Mini on 12/13/22.
+//
+
+import UIKit
+
+class selectViewController: UIViewController, UITextViewDelegate{
+    
+    
+    
+    @IBOutlet weak var SelectedsubReddit: UILabel!
+    @IBOutlet weak var Selectedusername: UILabel!
+    @IBOutlet weak var SelectedHeadline: UILabel!
+    @IBOutlet weak var SelectedContent: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
